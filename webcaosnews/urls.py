3b70b3b31d9ab7a251fb508f92cc1f3729ee4d70
Franko Vidal <francoborottov@gmail.com>
2023-04-19 14:07:14 -0400

    Index Visible

--- webcaosnews/urls.py
+++ webcaosnews/urls.py
@@ -2,6 +2,17 @@
 from django.contrib import admin
 from django.urls import path
 from .views import index
+from .views import economia
+from .views import internacional 
+from .views import politica
+from .views import reportaje
+from .views import deportes
+from .views import salud
+from .views import noticia1
+from .views import noticia2
+from .views import noticia3
+from .views import login
+from .views import registro
 
 #Ejemplo
 #path('',index,name='IND'),
@@ -12,4 +23,11 @@ from .views import index
 
 urlpatterns = [
     path('',index,name='IND'),
+    path('economia/',economia,name='ECO'),
+    path('internacional/',internacional,name='INT'),
+    path('politica/',politica,name='POL'),
+    path('reportajes/',reportaje,name='REP'),
+    path('salud/',salud,name='SAL'),
+    path('deportes/',deportes,name='DEP'),
+
 ]
